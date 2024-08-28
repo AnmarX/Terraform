b43af6ad2b41b207f9bdbd9ffe79bc49d6a0702b
Anmar <117080591+AnmarX@users.noreply.github.com>
2024-08-28 21:39:37 +0300

    .

--- main.tf
+++ main.tf
@@ -2,35 +2,28 @@
 terraform {
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
-      version = "4.0.1"
+      source  = "hashicorp/azurerm"
+      version = "3.116.0"
     }
   }
 }
 
 # 2. Configure the AzureRM Provider
 provider "azurerm" {
-  # The AzureRM Provider supports authenticating using via the Azure CLI, a Managed Identity
-  # and a Service Principal. More information on the authentication methods supported by
-  # the AzureRM Provider can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs#authenticating-to-azure
-
-  # The features block allows changing the behaviour of the Azure Provider, more
-  # information can be found here:
-  # https://registry.terraform.io/providers/hashicorp/azurerm/latest/docs/guides/features-block
   features {}
 }
 
 # 3. Create a resource group
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+resource "azurerm_resource_group" "test" {
+  name     = "test"
+  location = "Qatar Central"
+  
 }
 
-# 4. Create a virtual network within the resource group
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  address_space       = ["10.0.0.0/16"]
-}
+# # 4. Create a virtual network within the resource group
+# resource "azurerm_virtual_network" "example" {
+#   name                = "example-network"
+#   resource_group_name = azurerm_resource_group.example.name
+#   location            = azurerm_resource_group.example.location
+#   address_space       = ["10.0.0.0/16"]
+# }
