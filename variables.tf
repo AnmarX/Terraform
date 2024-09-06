dcaf2facd2a681fdd0a3d5de7a9cec3daea27c01
Anmar <117080591+AnmarX@users.noreply.github.com>
2024-09-06 15:10:46 +0300

    .

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "empty" {
-  description = "value"
+  description = "empty for now"
   type        = string
   #   you can write the value here on the default or put the value on the .tfvars
-  default = "value"
+  # default = "value"
 }
