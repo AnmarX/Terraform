178ee0850af76f2df9c52a202a7020a232a34d4a
AnmarX <anmarbably@gmail.com>
2024-09-08 20:03:39 +0300

    .

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import psutil
 import socket  # Import the socket module for AF_INET
-
+# this will work on windows
 def get_non_virtual_ip():
     # Loop over all network interfaces
     for interface_name, interface_addresses in psutil.net_if_addrs().items():
@@ -16,3 +16,19 @@ def get_non_virtual_ip():
 
 non_virtual_ip = get_non_virtual_ip()
 print(f"The IP address of the first non-virtual network interface is: {non_virtual_ip}")
+
+
+
+
+# this will work on the mac
+def get_local_ip():
+    # Connect to a remote host (doesn't matter if it's reachable)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.connect(("8.8.8.8", 80))  # Google's DNS
+    local_ip = sock.getsockname()[0]
+    sock.close()
+    return local_ip
+
+# Get the local IP and update the .tfvars file
+local_ip = get_local_ip()
+print(local_ip)
